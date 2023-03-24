18afbf5b894dec99910437603f1af97abf09d3c8
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-03-24 21:40:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
-
+import './App.css';
 function App() {
+  const [result,setResult]=useState("");
+ const clickHandler =(event)=>{
+ setResult(result.concat(event.target.value)) 
+ }
+ const clearDisplay=()=>{
+  setResult("");
+ }
+ const calculate=()=>{
+  setResult(eval(result).toString());
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    
+    <div className="cal-app"> 
+      <div className="cal-border">
+      <input type="text" placeholder="0" id="answer" value={result}/>
+      <input type="button" value="9" className="button" onClick={clickHandler}/>
+      <input type="button" value="8"className="button" onClick={clickHandler}/>
+      <input type="button" value="7" className="button" onClick={clickHandler}/>
+      <input type="button" value="6" className="button" onClick={clickHandler}/>
+      <input type="button" value="5" className="button" onClick={clickHandler}/>
+      <input type="button" value="4" className="button" onClick={clickHandler}/>
+      <input type="button" value="3" className="button" onClick={clickHandler}/>
+      <input type="button" value="2" className="button" onClick={clickHandler}/>
+      <input type="button" value="1" className="button" onClick={clickHandler}/>
+      <input type="button" value="0" className="button" onClick={clickHandler}/>
+      <input type="button" value="." className="button" onClick={clickHandler}/>
+      <input type="button" value="+" className="button" onClick={clickHandler}/>
+      <input type="button" value="-" className="button" onClick={clickHandler}/>
+      <input type="button" value="*" className="button" onClick={clickHandler}/>
+      <input type="button" value="/" className="button" onClick={clickHandler}/>
+      <input type="button" value="%" className="button" onClick={clickHandler}/>
+      
+      <input type="button" value="clear" className="button button1" onClick={clearDisplay}/>
+     
+      <input type="button" value="=" className="button button1" onClick={calculate}/>
+
+      <footer class="footer">
+    <p class="text">Created with <i class="fa fa-heart"></i> by Anjali </p>
+    <p>
+        <a href="https://www.linkedin.com/in/anjali-raj-5796a71a7" target="_blank" class="link"> 
+            <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+     
         </a>
-      </header>
+        <a href="https://github.com/AnjaliRaj05" target="_blank" class="link">
+            <i class="fa fa-github fa-2x" aria-hidden="true"></i>
+        </a>
+    </p>
+</footer>
+      </div>
     </div>
   );
 }
